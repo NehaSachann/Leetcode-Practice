2d498cbb5af4617932372f6f6d67075d3bd6591e
NehaSachann <115439977+NehaSachann@users.noreply.github.com>
2025-08-03 15:50:58 +0530

    Time: 1 ms (57.76%) | Memory: 44.3 MB (50.98%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int single_sum=0;
+        int double_sum=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]<10){
+                single_sum+=nums[i];
+            }else{
+                double_sum+=nums[i];
+            }
+        }
+        if(double_sum==single_sum){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
